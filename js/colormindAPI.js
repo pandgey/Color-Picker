d3172975bfa3962199c3afa9da79162091839dfb
pandgey <krittapong.ti@gmail.com>
2025-07-24 00:16:20 +1000

    added some comments in the main code

--- js/colormindAPI.js
+++ js/colormindAPI.js
@@ -2,6 +2,9 @@ var url = "http://colormind.io/api/";
 var data = {
 	model : "default",
 	input : [[44,43,44],[90,83,82],"N","N","N"]
+
+	// have to figure this out later
+	// input: [[44,43,44],[90,83,82],[191,157,175],[188,138,125],[215,170,66]]
 }
 
 var http = new XMLHttpRequest();
